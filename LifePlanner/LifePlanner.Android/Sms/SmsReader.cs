645beed6a7cea073b4830de4609ddca2a97227bc
Попов Федор Павлович <eynsherkiel@gmail.com>
2018-11-11 13:17:22 +0300

    Framework added

---
+++ LifePlanner/LifePlanner.Android/Sms/SmsReader.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using Android.App;
+
+namespace LifePlanner.Droid.Sms
+{
+    public class SmsReader
+    {
+        public string ReadSms()
+        {
+            var smsBuilder = new StringBuilder();
+            //var SMS_URI_INBOX = "content://sms/inbox";
+            var SMS_URI_ALL = "content://sms/";
+
+            var uri = Android.Net.Uri.Parse(SMS_URI_ALL);
+            var projection = new[] { "_id", "address", "person", "body", "date", "type" };
+            using (var cur = Application.Context.ContentResolver.Query(uri, projection, /*"address='123456789'"*/default, default, "date desc"))
+            {
+                if (cur.MoveToFirst())
+                {
+                    var index_Address = cur.GetColumnIndex("address");
+                    var index_Person = cur.GetColumnIndex("person");
+                    var index_Body = cur.GetColumnIndex("body");
+                    var index_Date = cur.GetColumnIndex("date");
+                    var index_Type = cur.GetColumnIndex("type");
+                    do
+                    {
+                        var strAddress = cur.GetString(index_Address);
+                        var intPerson = cur.GetInt(index_Person);
+                        var strbody = cur.GetString(index_Body);
+                        var longDate = cur.GetLong(index_Date);
+                        var int_Type = cur.GetInt(index_Type);
+
+                        smsBuilder.Append("[ ");
+                        smsBuilder.Append(strAddress + ", ");
+                        smsBuilder.Append(intPerson + ", ");
+                        smsBuilder.Append(strbody + ", ");
+                        smsBuilder.Append(longDate + ", ");
+                        smsBuilder.Append(int_Type);
+                        smsBuilder.Append(" ]\n\n");
+                    } while (cur.MoveToNext());
+
+                }
+                else
+                {
+                    smsBuilder.Append("no result!");
+                } // end if   
+
+                return smsBuilder.ToString();
+            }
+        }
+    }
+}
---
+++ LifePlanner/LifePlanner.Android/Sms/SmsReceiver.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Provider;
+using System;
+
+namespace LifePlanner.Droid.Sms
+{
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    [IntentFilter(new[] { "android.provider.Telephony.SMS_RECEIVED" })]
+    public class SmsReceiver : BroadcastReceiver
+    {
+        public static event Action<string> Sms;
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent.Action.Equals(Telephony.Sms.Intents.SmsReceivedAction))
+            {
+                var msgs = Telephony.Sms.Intents.GetMessagesFromIntent(intent);
+                foreach (var msg in msgs)
+                {
+                    //Log.Debug(TAG, $" MessageBody {msg.MessageBody}");
+                    //Log.Debug(TAG, $"DisplayOriginatingAddress {msg.DisplayOriginatingAddress}");
+                    //Log.Debug(TAG, $"OriginatingAddress {msg.OriginatingAddress}");
+
+                    Sms?.Invoke(msg.MessageBody);
+                }
+            }
+        }
+    }
+}
